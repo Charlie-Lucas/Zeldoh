4ad44d43bf2d48968605c81c9a07354d5086f2b9
Charlie <charlie@appventus.com>
2015-08-24 12:11:02 +0200

    generate app and api bundle

---
+++ src/Zeldoh/ApiBundle/ZeldohApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Zeldoh\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ZeldohApiBundle extends Bundle
+{
+}
---
+++ src/Zeldoh/AppBundle/ZeldohAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Zeldoh\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ZeldohAppBundle extends Bundle
+{
+}
