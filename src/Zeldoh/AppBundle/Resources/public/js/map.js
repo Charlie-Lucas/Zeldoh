a6851afe70be606fccbf64a8c667191f5be2ed6b
Theo-Turpin <theo.pierre@hotmail.fr>
2015-08-25 17:01:47 +0200

    Integration map

--- src/Zeldoh/AppBundle/Resources/public/js/map.js
+++ src/Zeldoh/AppBundle/Resources/public/js/map.js
@@ -2,11 +2,11 @@ function showZone(coord) {
 
 	var xy = coord.split(":");
 
-	var x = parseInt(coord[0]);
-	var y = parseInt(coord[1]);
+	var x = parseInt(xy[0]);
+	var y = parseInt(xy[1]);
 
-	var idxCol = Math.round(x/16);
-	var idxLine = Math.round(y/16);
+	var idxCol = Math.round(y/16);
+	var idxLine = Math.round(x/16);
 
 	$('li.areaLine:nth-child('+idxLine+')').find('ul.areas li.area:nth-child('+idxCol+')').addClass('showZone');
 }
