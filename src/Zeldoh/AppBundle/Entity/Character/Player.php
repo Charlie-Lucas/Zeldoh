31b0bb3390aee12ef66b06c7257828c0a2dbeb48
Charlie <charlie@appventus.com>
2015-08-26 17:01:22 +0200

    add a coordinate for a player

--- src/Zeldoh/AppBundle/Entity/Character/Player.php
+++ src/Zeldoh/AppBundle/Entity/Character/Player.php
@@ -12,5 +12,31 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Player extends Character
 {
+    /**
+     * @ORM\OneToOne(targetEntity="Zeldoh\AppBundle\Entity\Map\Coordinate")
+     */
+    private $coordinate;
 
+    /**
+     * Set coordinate
+     *
+     * @param \Zeldoh\AppBundle\Entity\Map\Coordinate $coordinate
+     * @return Player
+     */
+    public function setCoordinate(\Zeldoh\AppBundle\Entity\Map\Coordinate $coordinate = null)
+    {
+        $this->coordinate = $coordinate;
+
+        return $this;
+    }
+
+    /**
+     * Get coordinate
+     *
+     * @return \Zeldoh\AppBundle\Entity\Map\Coordinate 
+     */
+    public function getCoordinate()
+    {
+        return $this->coordinate;
+    }
 }
