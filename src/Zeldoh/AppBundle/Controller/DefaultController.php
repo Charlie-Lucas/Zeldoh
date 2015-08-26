c029df7c6caf16b2ea6d8911e0ef3f292b0d611a
Charlie <charlie@appventus.com>
2015-08-26 13:25:39 +0200

    return a player in controller

--- src/Zeldoh/AppBundle/Controller/DefaultController.php
+++ src/Zeldoh/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Zeldoh\AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Zeldoh\AppBundle\Entity\Character\Player;
 
 class DefaultController extends Controller
 {
@@ -14,6 +15,9 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return array('map' => $this->get('zeldoh.map.loader')->import());
+        return array(
+            'map' => $this->get('zeldoh.map.loader')->import(),
+            'player' => new Player()
+        );
     }
 }
