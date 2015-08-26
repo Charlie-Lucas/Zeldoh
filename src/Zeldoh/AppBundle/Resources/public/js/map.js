1797dd15149740532d4c4b24ad1010c33dc55e9d
Theo-Turpin <theo.pierre@hotmail.fr>
2015-08-26 08:34:25 +0200

    JS map

--- src/Zeldoh/AppBundle/Resources/public/js/map.js
+++ src/Zeldoh/AppBundle/Resources/public/js/map.js
@@ -5,8 +5,24 @@ function showZone(coord) {
 	var x = parseInt(xy[0]);
 	var y = parseInt(xy[1]);
 
+	var nbLine = $('li.areaLine').length;
+	var nbCol = $('ul.areas li.area').length/nbLine;
+
 	var idxCol = Math.round(y/16);
+	if (idxCol <= 0) {
+		idxCol++;
+	}
+	else if (idxCol > nbCol) {
+		idxCol--;
+	}
+
 	var idxLine = Math.round(x/16);
+	if (idxLine <= 0) {
+		idxLine++;
+	}
+	else if (idxLine > nbLine) {
+		idxLine--;
+	}
 
 	$('li.areaLine:nth-child('+idxLine+')').find('ul.areas li.area:nth-child('+idxCol+')').addClass('showZone');
 }
