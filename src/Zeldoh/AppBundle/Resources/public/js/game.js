fa74dc6d283025155f10cc3f5fc5c2618f355570
Charlie <charlie@appventus.com>
2015-08-26 17:01:53 +0200

    add move for player

--- src/Zeldoh/AppBundle/Resources/public/js/game.js
+++ src/Zeldoh/AppBundle/Resources/public/js/game.js
@@ -1,18 +1,49 @@
 var Game = function(data){
-    this.player = data.player;
+    this.player = new Player(data.player);
     this.map = data.map;
     this.spawn= null;
+    var maxX = 0;
+    var maxY = 0;
     this.currentArea = null;
     var self = this;
     this.start = function(){
-        this.setSpawn($('.spawn'));
-    }
+        var spawn = $('.spawn');
+        this.setSpawn(spawn);
+        this.showCurrentZone(spawn);
+        this.player.setPosition(spawn);
+        this.player.setGame(this);
+
+        window.addEventListener('keydown', function(event){
+            //if (event.defaultPrevented) {
+            //    return; // Should do nothing if the key event was already consumed.
+            //}
+            switch (event.keyCode) {
+                case 40:
+                    self.player.moveDown();
+                    break;
+
+                case 38:
+                    self.player.moveUp();
+                    break;
+                case 37:
+                    self.player.moveLeft();
+
+                    break;
+                case 39:
+                    self.player.moveRight();
+                    break;
+                default:
+                    return;
+            }
+
+
+            event.preventDefault();
+        })
+    };
     this.setSpawn = function(spawn){
         this.spawn = spawn;
-        this.showZone(spawn);
-        console.log(spawn.data('x') + ':' + spawn.data('y'));
-    }
-    this.showZone = function(coordinate) {
+    };
+    this.showCurrentZone = function(coordinate) {
         if(this.currentArea != null){
             this.currentArea.removeClass('showZone');
         }
@@ -20,4 +51,59 @@ var Game = function(data){
         this.currentArea = area;
         area.addClass('showZone')
     }
-}
+    this.setMaxY = function(maxY)
+    {
+        this.maxY = maxY;
+    }
+    this.setMaxX = function (maxX) {
+        this.maxX = maxX;
+    }
+};
+var Player = function(data){
+    this.game = null;
+    this.data = data;
+    this.position = null;
+    this.setPosition = function (position){
+        if(position.hasClass('land'))
+        {
+
+            if(this.position != null)
+            {
+                if(position.parents('.area') != this.position.parents('.area')){
+                    this.game.showCurrentZone(position);
+                }
+                this.position.removeClass('player');
+            }
+            this.position = position;
+            this.position.addClass('player');
+        }
+    }
+    this.setGame = function(game){
+        this.game = game;
+    }
+    this.moveDown = function(){
+        if(this.position.data('x') + 1 <= this.game.maxX) {
+            var newPositionId = "coordinate_" + (this.position.data('x') + 1) + '_' + this.position.data('y');
+            this.setPosition($('#' + newPositionId));
+        }
+    }
+    this.moveUp = function(){
+        if(this.position.data('x') - 1 >= 0)
+        {
+            var newPositionId = "coordinate_" + (this.position.data('x') - 1) + '_'+ this.position.data('y');
+            this.setPosition($('#' + newPositionId));
+        }
+    }
+    this.moveLeft = function(){
+        if(this.position.data('y') - 1 >= 0) {
+            var newPositionId = "coordinate_" + this.position.data('x') + '_' + (this.position.data('y') - 1);
+            this.setPosition($('#' + newPositionId));
+        }
+    }
+    this.moveRight = function(){
+        if(this.position.data('y') + 1 <= this.game.maxY) {
+            var newPositionId = "coordinate_" + this.position.data('x') + '_' + (this.position.data('y') + 1);
+            this.setPosition($('#' + newPositionId));
+        }
+    }
+};
